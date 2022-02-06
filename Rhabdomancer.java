36d1076fdf21dcabd00c13515cf55aa6a9639f28
raptor <raptor@0xdeadbeef.info>
2022-02-06 10:43:26 +0100

    Add open64 family functions

--- Rhabdomancer.java
+++ Rhabdomancer.java
@@ -122,7 +122,7 @@ public class Rhabdomancer extends GhidraScript
 			"execl", "execlp", "execle", "execv", "execve", "execvp", "execvpe",
 			"system", "fork", "pipe", "popen",
 			// i/o functions can be used insecurely
-			"open", "openat", "fopen", "freopen", "dlopen",
+			"open", "open64", "openat", "openat64", "fopen", "fopen64", "freopen", "freopen64", "dlopen",
 			"read", "fread", // check read from unreadable paths/files and from writable paths/files
 			"write", "fwrite", // check write to unwritable paths/files
 			"recv", "recvfrom", // check for null-termination
